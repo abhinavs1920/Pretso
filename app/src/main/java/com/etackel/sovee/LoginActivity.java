d748edce3765637747752946a0ce741e9e5917c3
abhinavs1920 <113504774+abhinavs1920@users.noreply.github.com>
2023-01-21 18:46:46 +0530

    Initial commit

---
+++ app/src/main/java/com/etackel/sovee/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.etackel.sovee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
